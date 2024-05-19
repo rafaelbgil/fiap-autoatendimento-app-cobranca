85b3af8b9e9d1300e0ee55dc42ecf86e528a8fa2
Rafael Benite Gil <rafaelbgil@gmail.com>
2024-05-19 17:20:14 -0300

    corrigindo vulnerabilidade

--- cobranca/settings.py
+++ cobranca/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from os import environ
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k68sm3b!(tc5q0v+0#rqnpw-#gz)gcqk$rrwy9h2!xvfzymzb*'
+SECRET_KEY = os.environ.get('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
