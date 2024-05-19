aa809fcde56f2996e0758ba8c8428f4d266bca08
Rafael Benite Gil <rafaelbgil@gmail.com>
2024-05-19 16:30:45 -0300

    adicionado githubactions com sonarcloud

--- api/tests.py
+++ api/tests.py
@@ -3,4 +3,3 @@ from tests.utils.TestUtils import *
 from tests.domain.entities.TestCobranca import *
 from tests.external.TestAutoMeioPagamento import TestAutoMeioPagamento
 from tests.external.TestMercadoPagoMeioPagamento import TestMercadoPagoMeioPagamento
-
